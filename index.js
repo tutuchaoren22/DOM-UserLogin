6f7005476b60227063a72e3ff879eda4306fb6cb
duyuqian <15738381799@163.com>
2019-12-19 18:57:53 +0800

    [duyuqian] fix:check and modify name of class and var

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.onload = function() {
         if (userName.value == "123" && passWord.value == "123") {
             alert("登录成功！");
         } else {
-            alert("账号或密码输入错误！");
+            alert("账号或密码输入错误,请重新输入！");
             passWord.value = '';
         }
     }
