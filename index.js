c6ee16fdfa861bee6d913ee3d495112c898225f1
duyuqian <15738381799@163.com>
2019-12-21 23:13:34 +0800

    [duyuqian] refactor: put the js behind HTML body & put onclick event in HTML not in JS

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-window.onload = function() {
+function userLogin() {
     var buttonOfLogin = document.getElementById("login");
-    var userName = document.getElementById("username");
-    var passWord = document.getElementById("password");
-    buttonOfLogin.onclick = function() {
-        if (userName.value == "123" && passWord.value == "123") {
-            alert("登录成功！");
-        } else {
-            alert("账号或密码输入错误,请重新输入！");
-            passWord.value = '';
-        }
+    var userName = document.getElementsByClassName("username")[0];
+    var passWord = document.getElementsByClassName("password")[0];
+    if (userName.value === "123" && passWord.value === "123") {
+        alert("登录成功！");
+    } else {
+        alert("账号或密码输入错误,请重新输入！");
+        passWord.value = '';
     }
 }
